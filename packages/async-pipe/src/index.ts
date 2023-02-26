ad1bd0180ffea3bda1658460d778aa0fba4fbcf3
Iago Belo <iagobelo.oliveira@gmail.com>
2023-02-26 09:14:00 -0300

    feat(async-pipe): working async pipe

--- packages/async-pipe/src/index.ts
+++ packages/async-pipe/src/index.ts
@@ -1,13 +1,29 @@
 import { isFunction, isPromise } from '@ze-ts/utils';
 
 /**
- * A type that represents a function that takes a value of type `A` checks if it is a promise and if so unwraps it.
- *
+ * A type that represents a function that takes a value of
+ * type `A` checks if it is a promise and if so unwraps it.
  */
 type Fn<A, B> = (a: A extends Promise<infer T> ? T : A) => B;
 
+/**
+ * A type alias for a function that takes an unknown value
+ * and returns an unknown value.
+ */
 type UnknownFn = Fn<unknown, unknown>;
 
+/**
+ * A type that represents the result of a pipeAsync call.
+ * It will be the last type in the list of functions.
+ * If the last function returns a promise, the result will
+ * be the type of the promise.
+ * @example
+ * pipeAsync(1, (v) => v + 1, (v) => v + 2, (v) => Promise.resolve(v + 3))
+ * // will return a Promise<number> with the value 7
+ * @example
+ * pipeAsync(1, (v) => v + 1, (v) => v + 2, (v) => v + 3)
+ * // will return a number with the value 6
+ */
 type AsyncPipeResultType<T extends unknown[]> = T extends [
   infer C,
   ...infer Rest
@@ -20,8 +36,20 @@ type AsyncPipeResultType<T extends unknown[]> = T extends [
     : AsyncPipeResultType<Rest>
   : never;
 
+interface PipeAsyncThis {
+  __callCount?: number;
+}
+
+/**
+ * A type that represents a pipeAsync function.
+ * It takes a value and a list of functions and returns
+ * the result of the composition of the functions.
+ * @example
+ * pipeAsync(1, (v) => v + 1, (v) => v + 2, (v) => v + 3)
+ * // will return a number with the value 6
+ */
 // prettier-ignore
-interface PipeAsync {
+interface PipeAsync extends PipeAsyncThis {
   <A, B>(v: A, fn1: Fn<A, B>): AsyncPipeResultType<[A, B]>;
   <A, B, C>(v: A, fn1: Fn<A, B>, fn2: Fn<B, C>): AsyncPipeResultType<[A, B, C]>;
   <A, B, C, D>(v: A, fn1: Fn<A, B>, fn2: Fn<B, C>, fn3: Fn<C, D>): AsyncPipeResultType<[A, B, C, D]>;
@@ -42,15 +70,21 @@ interface PipeAsync {
  * @param params - The list of functions to compose. The first argument should be a value.
  * @returns The result of the composition.
  */
-export const pipeAsync: PipeAsync = (
+const pipeAsync: PipeAsync = function (
+  this: PipeAsyncThis,
   ...[value, fn, ...fns]: [unknown, ...UnknownFn[]]
-) => {
+) {
   if (isFunction(fn)) {
     const nextValue = isPromise(value) ? value.then(fn) : fn(value);
-    return pipeAsync(nextValue, ...(fns as [UnknownFn]));
+    return pipeAsync.apply({ __callCount: (this?.__callCount ?? 0) + 1 }, [
+      nextValue,
+      ...fns,
+    ]);
   }
 
   return value;
 };
 
 pipeAsync.__tag = 'pipe-async';
+
+export default pipeAsync;
